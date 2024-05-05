9ad93875759a56aa7af128e8d2024ab5ff774326
Avyudaya Acharya <113654352+avyudaya@users.noreply.github.com>
2024-05-05 08:35:27 +0545

    forms:localstorage and bootstrap

--- forms/script.js
+++ forms/script.js
@@ -1,10 +1,32 @@
 let notes = [];
+let x = localStorage.getItem("notes")
+if(x != null) {
+    notes = JSON.parse(x);
+}
+
 let form = document.getElementById('create-note-form');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     
     let newNote = document.getElementById('new-note').value;
+    if(newNote === ""){
+        return;
+    }  // if the note is empty don't add it to the list
     notes.push(newNote);
-    console.log(notes);
+    showNotes();
+
+    document.getElementById('new-note').value = "";
 })
+
+showNotes();
+function showNotes(){
+    let ulTag = document.getElementById("notes");
+    let html = "";
+
+    notes.forEach((note, index) => {
+        html += `<li class="list-group-item">${note}</li>`
+    })
+
+    ulTag.innerHTML = html;
+}
