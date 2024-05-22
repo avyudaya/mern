3e9bc3b9dad57a053d641ddb2546b851108795db
avyudaya <avyudaya7@gmail.com>
2024-05-22 08:31:21 +0545

    component state

--- 8. react-starting/component-state/src/App.jsx
+++ 8. react-starting/component-state/src/App.jsx
@@ -1,17 +1,23 @@
-import {useState} from 'react'
+import { useState } from "react";
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
 
-  const [counter, setCounter] = useState(5);
+  const increaseClick = () => {
+    setCounter(counter+1)
+  };
 
-  setTimeout(() => {
-    setCounter(100);
-  }, 1000)
+  const decreaseClick = () => {
+    setCounter(counter-1)
+  }
 
   return (
-    <div>{counter}</div>
-  )
-}
+    <div>
+      <div>{counter}</div>
+      <button onClick={increaseClick}>plus</button>
+      <button onClick={decreaseClick}></button>
+    </div>
+  );
+};
 
-
-export default App
+export default App;
