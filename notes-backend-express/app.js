6ffcf43844b83e2ca83b55ed0a7034433dc76a77
avyudaya <avyudaya1@gmail.com>
2024-07-04 08:09:07 +0545

    tests backend

--- notes-backend-express/app.js
+++ notes-backend-express/app.js
@@ -17,6 +17,7 @@ mongoose.connect(config.MONGODB_URL).then(() => {
     logger.error('error connecting to db', err.message);
 })
 
+app.use(express.static('dist'))
 app.use(cors());
 app.use(express.json());
 app.use(middleware.requestMiddleware);
